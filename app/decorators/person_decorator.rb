809c57789fff89f072c0a71b0de74fa5b80587e9
Maciek <maciej.jarmula@gmail.com>
2016-12-22 10:55:23 +0100

    Add birthdate to Student
    This commit adds migration for birthdate column,
    adds validation for birthdate range in Student model
    adds formatted_birthdate in person decorator,
    adds langauge variable to locales en
    adds field birthdate to students form partial
    removes css for student table in reports
    adds birthdate to student_params
    adds birthdate to table in reports subject_item partial

--- app/decorators/person_decorator.rb
+++ app/decorators/person_decorator.rb
@@ -2,4 +2,8 @@ class PersonDecorator < BaseDecorator
   def full_name
     "#{first_name} #{last_name}"
   end
+  
+  def formatted_birthdate
+    birthdate.try(:strftime, "%Y-%m-%d") || "---"
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,5 +4,13 @@ class Student < ActiveRecord::Base
   has_many :subject_item_notes, dependent: :destroy
 
   validates :first_name, :last_name, presence: true
+  validate :birthdate_in_valid_range
+                                 
   accepts_nested_attributes_for :subject_items
+  
+  protected
+  
+  def birthdate_in_valid_range
+    self.errors.add(:birthdate, "is not valid") unless ((Time.now - 70.years)..(Time.now - 16.years)).include?(self.birthdate) and self.birthdate.present?
+  end
 end
